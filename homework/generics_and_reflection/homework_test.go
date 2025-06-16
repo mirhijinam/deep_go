67a06f113f1917ea1b076070b474d0b51f00e089
Atabekov Ibragim <atabekov.ibragim@wb.ru>
2025-06-17 01:48:26 +0300

    hw9: done

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"slices"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,15 +13,43 @@ import (
 // go test -v homework_test.go
 
 type Person struct {
-	Name    string `properties:"name"`
+	Name    string `properties:"nameOfField"`
 	Address string `properties:"address,omitempty"`
 	Age     int    `properties:"age"`
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+func Serialize[T any](obj T) string {
+	var lines []string
+
+	objType := reflect.TypeOf(obj)
+	objVal := reflect.ValueOf(obj)
+
+	for i := 0; i < objType.NumField(); i++ {
+		field := objType.Field(i)
+
+		tagString := field.Tag.Get("properties")
+		if tagString == "" {
+			continue
+		}
+
+		tagParts := strings.Split(tagString, ",")
+		nameOfField := tagParts[0]
+		if nameOfField == "" {
+			continue
+		}
+
+		value := objVal.Field(i)
+		isOmitempty := slices.Contains(tagParts, "omitempty")
+		if isOmitempty && value.IsZero() {
+			continue
+		}
+
+		line := fmt.Sprintf("%s=%v", nameOfField, value.Interface())
+		lines = append(lines, line)
+	}
+
+	return strings.Join(lines, "\n")
 }
 
 func TestSerialization(t *testing.T) {
@@ -26,7 +58,7 @@ func TestSerialization(t *testing.T) {
 		result string
 	}{
 		"test case with empty fields": {
-			result: "name=\nage=0\nmarried=false",
+			result: "nameOfField=\nage=0\nmarried=false",
 		},
 		"test case with fields": {
 			person: Person{
@@ -34,21 +66,21 @@ func TestSerialization(t *testing.T) {
 				Age:     30,
 				Married: true,
 			},
-			result: "name=John Doe\nage=30\nmarried=true",
+			result: "nameOfField=John Doe\nage=30\nmarried=true",
 		},
-		"test case with omitempty field": {
+		"test case with isOmitempty structField": {
 			person: Person{
 				Name:    "John Doe",
 				Age:     30,
 				Married: true,
 				Address: "Paris",
 			},
-			result: "name=John Doe\naddress=Paris\nage=30\nmarried=true",
+			result: "nameOfField=John Doe\naddress=Paris\nage=30\nmarried=true",
 		},
 	}
 
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
+	for nameOfField, test := range tests {
+		t.Run(nameOfField, func(t *testing.T) {
 			result := Serialize(test.person)
 			assert.Equal(t, test.result, result)
 		})
